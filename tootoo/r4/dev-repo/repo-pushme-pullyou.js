a657b3dc7f8faff62a2e9759c294942637617d8d
Theo Armour <t.armour@gmail.com>
2017-05-29 14:04:02 -0700

    add pp js

---
+++ tootoo/r4/dev-repo/repo-pushme-pullyou.js
@@ -0,0 +1,33 @@
+
+	let user = {
+		user : 'pushme-pullyou',
+		subText : 'Browse & view stuff on GitHub real fast!',
+		repo : 'pushme-pullyou.github.io',
+		branch : 'master',
+		folder : '',
+	};
+
+
+	MNU.tableOfContents =
+`### Pushme-Pullyou
+* [Read Me]( #README.md )
+### GubGub
+* [Read Me]( #gubgub/README.md )
+### DabDab
+* [Read Me]( #dabdab/README.md )
+### TooToo
+* [TooToo Read Me]( #tootoo/README.md )
+#### TooToo Versions
+* [Dev Read Me]( #tootoo/r3/dev/README.md )
+* [Dev Choose Read Me]( #tootoo/r3/dev-choose/README.md )
+* [Dev Custom Read Me]( #tootoo/r3/dev-custom/README.md )
+* [Dev Select Read Me]( #tootoo/r3/dev-select/README.md )
+* [All in One Read Me]( #tootoo/r3/all-in-one/README.md )
+* [All in One Custom Read Me]( #tootoo/r3/all-in-one-custom/README.md )
+#### TooToo Utilities
+* [Utilities Read Me]( #tootoo/r3/utilities/README.md )
+* [get-github-dir-names]( #tootoo/r3/utilities/get-github-dir-names/README.md )
+* [Make Read Me Read Me]( #tootoo/r3/utilities/make-github-readme/README.md )
+* [Merge Files into one]( #tootoo/r3/utilities/merge-files-into-one/README.md )
+
+`;
