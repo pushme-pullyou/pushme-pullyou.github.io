c1274cb7bc7c1a7a40fff1240a53c0392ab4e7d9
Theo Armour <t.armour@gmail.com>
2017-06-04 19:56:05 -0700

    lb

--- tootoo/r4/dev-repo/repo-ladybug-web.js
+++ tootoo/r4/dev-repo/repo-ladybug-web.js
@@ -42,9 +42,31 @@
 #### Shadow OBJ Core
 * [Shadow OBJ Core Read Me]( #shadow-obj-core/readme.md )
 * [Shadow OBJ Core ]( #shadow-obj-core/ladybug-web-shadow-obj-core-r3.html )
+* &#x1F5D7; [Shadow OBJ Core ]( https://rawgit.com/ladybug-tools/ladybug-web/gh-pages/shadow-obj-core/ladybug-web-shadow-obj-core-r3.html )
 #### Shadow OBJ Gallery
 * [Shadow OBJ Gallery Read Me]( #shadow-obj-gallery/readme.md )
 * [Shadow OBJ Gallery ]( #shadow-obj-gallery/ladybug-web-shadow-obj-gallery-r3.html )
+* &#x1F5D7; [Shadow OBJ Gallery ]( https://rawgit.com/ladybug-tools/ladybug-web/gh-pages/shadow-obj-gallery/ladybug-web-shadow-obj-gallery-r3.html )
+#### Shadow OBJ Opener
+* [Shadow OBJ Opener Read Me]( #shadow-obj-opener/readme.md )
+* [Shadow OBJ Opener ]( #shadow-obj-opener/ladybug-web-shadow-obj-opener-r2.html )
+* &#x1F5D7; [Shadow OBJ Opener ]( https://rawgit.com/ladybug-tools/ladybug-web/gh-pages/shadow-obj-opener/ladybug-web-shadow-obj-opener-r2.html )
+
+### Shadow Range
+#### Shadow Range OBJ Core
+* [Shadow Range OBJ Core Read Me]( #shadow-range-obj-core/readme.md )
+* [Shadow Range OBJ Core ]( #shadow-range-obj-core/ladybug-web-shadow-range-obj-core-r4.html )
+* &#x1F5D7; [Shadow Range OBJ Core ]( https://rawgit.com/ladybug-tools/ladybug-web/gh-pages/shadow-range-obj-core/ladybug-web-shadow-range-obj-core-r4.html )
+#### Shadow OBJ Gallery
+* [Shadow Range OBJ Gallery Read Me]( #shadow-range-range-obj-gallery/readme.md )
+* [Shadow Range OBJ Gallery ]( #shadow-range-obj-gallery/ladybug-web-shadow-range-obj-gallery-r5.html )
+* &#x1F5D7; [Shadow Range OBJ Gallery ]( https://rawgit.com/ladybug-tools/ladybug-web/gh-pages/shadow-range-obj-gallery/ladybug-web-shadow-range-obj-gallery-r5.html )
+#### Shadow OBJ Opener
+* [Shadow Range OBJ Opener Read Me]( #shadow-range-obj-opener/readme.md )
+* [Shadow Range OBJ Opener ]( #shadow-range-obj-opener/ladybug-web-shadow-range-obj-opener-r2.html )
+* &#x1F5D7; [Shadow Range OBJ Opener ]( https://rawgit.com/ladybug-tools/ladybug-web/gh-pages/shadow-range-obj-opener/ladybug-web-shadow-range-obj-opener-r2.html )
+
+
 `;
 
 	CON.massageText = function( response ){
