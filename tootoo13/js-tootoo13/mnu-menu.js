5cf44a55525415740eaedf6c338bc69f9e450502
Theo Armour <theo-armour@users.noreply.github.com>
2019-01-09 16:53:32 -0800

    many fixes

--- tootoo13/js-tootoo13/mnu-menu.js
+++ tootoo13/js-tootoo13/mnu-menu.js
@@ -3,9 +3,9 @@
 /* jshint esversion: 6 */
 
 
-const MNU = { "release": "R13.1", "date": "2019-01-06" };
+const MNU = { "release": "R13.2", "date": "2019-01-09" };
 
-MNU.description = MNU.descriptionCore || document.head.querySelector( "[ name=description ]" ).content;
+MNU.description = document.head.querySelector( "[ name=description ]" ).content;
 
 MNU.currentStatusCore =
 	`
@@ -14,9 +14,9 @@ MNU.currentStatusCore =
 
 MNU.currentStatusMenu =
 	`
-		<h3>MNU ${ MNU.release} status ${ MNU.date }</h3>
+		<h3>TooToo Menu ${ MNU.release} ~ ${ MNU.date }</h3>
 
-		<p>TooToo Core Menu.</p>
+		<p>Generates HTML menu content from variables in the index.html file.</p>
 
 		<p>
 			Concept
@@ -31,11 +31,13 @@ MNU.currentStatusMenu =
 
 		<p>This module is ready for light testing.</p>
 
-		<p><a href="#/README.md" target="_blank">MNU Read Me</a></p>
+		<p><a href="https://github.com/pushme-pullyou/pushme-pullyou.github.io/tree/master/tootoo13/cookbook-testing/mnu-menu" target="_blank" >TooToo Menu Read Me</a></p>
 
 		<p>
 			Change log
 			<ul>
+				<li>2019-01-09 ~ Content update and minor code fixes</li>
+				<li>Add vars: MNU.footerUrl, MNU.footerTarget, MNU.footerIssues</li>
 				<li>2018-12-29 ~ Add helpItem class</li>
 				<li>2018-12-28 ~ Current Status link changed to question mark</li>
 				<li>2018-12-28 ~ Content displayed in the Pop-Up</li>
@@ -64,8 +66,11 @@ MNU.appUrl = "";
 
 MNU.urlSourceCodeImage = "https://status.github.com/images/invertocat.png";
 MNU.urlSourceCodeIcon = `<img src="${ MNU.urlSourceCodeImage }" height=18 >`;
-MNU.urlSourceCodeUrl = "https://github.com/pushme-pullyou/pushme-pullyou.github.io/tree/master/tootoo-templates/hamburger-theme-cms";
+MNU.urlSourceCodeUrl = "https://github.com/pushme-pullyou/pushme-pullyou.github.io/tree/master/tootoo13/cookbook-testing/mnu-menu";
 
+MNU.footerUrl = "#";
+MNU.footerTarget = ""; //"target=_blank";
+MNU.footerIssues = "https://github.com/pushme-pullyou/pushme-pullyou.github.io/issues";
 
 MNU.xDown = null;
 MNU.yDown = null;
@@ -79,26 +84,24 @@ MNU.getNavHeader = function() {
 	document.addEventListener( 'touchstart', MNU.onTouchStart, false );
 	document.addEventListener( 'touchmove', MNU.onTouchMove, false );
 
-	const path = "../../../index.html";
-
 	const htm  =
 	`
 		<h3>
 		<a href="${ MNU.homeUrl }" title="${ MNU.homeTitle }" target="_top">
 		${ MNU.homeText }
 		</a>
-		&raquo;
+		${ MNU.homeUrl ? '&raquo;' : '' }
 		<a href="${ MNU.repoUrl }" title="${ MNU.repoTitle }" target="_top">
 		${ MNU.repoText }
 		</a>
-		&raquo;
+		${ MNU.repoUrl ? '&raquo;' : '' }
 		<a href="${ MNU.appUrl }" title="${ MNU.appTitle }" >
 		${ MNU.appText }
 		</a>
 		${ MNU.appUrl ? '&raquo;' : '' }
 		</h3>
 		<h2>
-			<a href=${ MNU.urlSourceCodeUrl } target="_blank" title="Source code on GitHub" >
+			<a href=${ MNU.urlSourceCodeUrl } ${ MNU.footerTarget } title="Source code on GitHub" >
 			${ MNU.urlSourceCodeIcon }
 			</a>
 			<a href="" title="Click to reload this page" >${ document.title }</a>
@@ -108,22 +111,11 @@ MNU.getNavHeader = function() {
 			</a>
 		</h3>
 
-		<p>
-			${ MNU.description }
-			<a id=mnuHead class=helpItem href="JavaScript:MNU.setPopupShowHide(mnuHead,MNU.currentStatusMenu);"
-				title="Current status: core module" >&nbsp; ? &nbsp;
+		<div>
+			${ MNU.description } <a id=mnuHead class=helpItem href="JavaScript:MNU.setPopupShowHide(mnuHead,MNU.currentStatusMenu);"
+				title="Current status" >&nbsp; ? &nbsp;
 			</a>
-
-		<!--
-			 ${ document.head.querySelector( '[ name=description ]' ).content }
-			 Open, examine and edit very large <a href="http://gbxml.org" target="_blank"  title="Thank you, Stephen">gbXML</a> files in 3D in your browser with free, open source
-			 <a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank"  title="Thank you, Brendan">JavaScript</a>,
-			 <a href="https://en.wikipedia.org/wiki/WebGL" target="_blank" title="Thank you, Ken" >WebGL</a> &
-			 <a href="https://en.wikipedia.org/wiki/Three.js" target="_blank" title="Thank you, Ricardo" >Three.js</a>
-			 <a class=helpItem href="JavaScript:MNU.setPopupShowHide(this,MNU.currentStatus);"
-			 title="Current status: menu MNU module" >&nbsp; ? &nbsp;</a>
-		-->
-		</p>
+		</div>
 	`;
 
 	return htm;
@@ -134,25 +126,25 @@ MNU.getNavHeader = function() {
 
 MNU.getNavFooter = function() {
 
-	const path = "../../../index.html#";
+
 	const htm  =
 	`
 		<details>
 
 			<summary>Footer / Help
-				<a id=mnuFoot class=helpItem href="JavaScript:MNU.setPopupShowHide(mnuFoot,MNU.currentStatus);" >&nbsp; ? &nbsp;</a>
+				<a id=mnuFoot class=helpItem href="JavaScript:MNU.setPopupShowHide(mnuFoot,MNU.currentStatusMenu);" >&nbsp; ? &nbsp;</a>
 			</summary>
 
-			<div style=margin-top:1rem; title='What is this stuff?' ><a href=${ path }pages/about-spider-code-style.md target="_blank" >Coding style</a></div>
-			<div title='many thanks!' ><a href=${ path }pages/credits.md target="_blank" >Credits</a></div>
-			<div><a href=${ path }pages/code-of-conduct.md target="_blank" >Code of conduct</a></div>
-			<div><a href=${ path }pages/contributing.md target="_blank" >Contributing via GitHub</a></div>
-			<div><a href=${ path }pages/license.md target="_blank" >MIT License</a></div>
-			<div><a href=${ path }pages/markdown-help.md target="_blank" >Markdown help</a></div>
-			<div><a href=${ path }pages/themes.md target="_blank" >Themes help</a></div>
-			<div><a title='Need help' href=https://github.com/ladybug-tools/spider-gbxml-tools/issues target=_blank >GitHub Issues</a></div>
-			<div><a href="javascript:( function(){ var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='https://rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})()" title="Mr.doob's Stats.js appear in top left MNUner" >Show frames/second statistics</a></div>
-			<div><a href="https://api.github.com/rate_limit" title='If menu stops appearing, it is likely due to too many API calls' target=_blank >View GitHub API rate limits</a></div>
+			<div style=margin-top:1rem; title='What is this stuff?' ><a href=${ MNU.footerUrl }pages/about-spider-code-style.md ${ MNU.footerTarget } >Coding style</a></div>
+			<div title='many thanks!' ><a href=${ MNU.footerUrl }pages/credits.md ${ MNU.footerTarget } >Credits</a></div>
+			<div><a href=${ MNU.footerUrl }pages/code-of-conduct.md ${ MNU.footerTarget } >Code of conduct</a></div>
+			<div><a href=${ MNU.footerUrl }pages/contributing.md ${ MNU.footerTarget } >Contributing via GitHub</a></div>
+			<div><a href=${ MNU.footerUrl }pages/license.md ${ MNU.footerTarget } >MIT License</a></div>
+			<div><a href=${ MNU.footerUrl }pages/markdown-help.md ${ MNU.footerTarget } >Markdown help</a></div>
+			<div><a href=${ MNU.footerUrl }pages/themes.md ${ MNU.footerTarget } >Themes help</a></div>
+			<div>&raquo; <a title='Need help' href=${ MNU.footerIssues } target=_blank >GitHub Issues</a></div>
+			<div>! <a href="javascript:( function(){ var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='https://rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})()" title="Mr.doob's Stats.js appear in top left corner" >Show frames/second statistics</a></div>
+			<div>&raquo; <a href="https://api.github.com/rate_limit" title='If files and folder stop appearing, it is likely due to too many API calls' target=_blank >View GitHub API rate limits</a></div>
 
 			<hr>
 
